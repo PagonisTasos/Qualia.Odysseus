8ab650667c815e9b94f8ac062ab34cbbe389607c
PagonisTasos <apagon@protonmail.com>
2022-04-20 00:26:34 +0300

    DateTime - sql safe date (>1753)

---
+++ Qualia.Odysseus/System.DateTime/DateTime.IsSqlSafe.cs
@@ -0,0 +1,12 @@
+﻿public static partial class Extensions
+{
+    /// <summary>
+    ///     An extension method that checks if the value is greater than 1753/01/01.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>True if the value is greater than 1753/01/01.</returns>
+    public static bool IsSqlSafe(this DateTime @this)
+    {
+        return @this.CompareTo(new DateTime(1753, 1, 1)) == -1;
+    }
+}
